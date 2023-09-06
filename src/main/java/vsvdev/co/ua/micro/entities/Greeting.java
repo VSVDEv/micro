04a1813afdd010055aebb9d69211d9a65deecf1c
serhii voronkov <voronkovserhii@gmail.com>
2023-09-06 17:37:48 +0300

    Initial commit

---
+++ src/main/java/vsvdev/co/ua/micro/entities/Greeting.java
@@ -0,0 +1,13 @@
+package vsvdev.co.ua.micro.entities;
+
+public class Greeting {
+
+    public long id;
+    public String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+}
